820de5b45f8fe9d20148273b81406cab4a1aac26
Ashin Sabu <139749674+ashinsabu3@users.noreply.github.com>
2025-02-23 18:05:31 +0530

    server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,25 @@
-package server
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/ashinsabu/ash-dis/server"
+)
+
+func main() {
+	listenPort := flag.Int("port", 6379, "Port to run the Ashdis server on")
+	listenAddr := flag.String("addr", "localhost", "Address to run the Ashdis server on")
+	flag.Parse()
+
+	fmt.Printf("Starting Ashdis server on %s:%d\n", *listenAddr, *listenPort)
+	ashidisServer := server.NewAshdisServer(server.AshdisServerOpts{
+		ListenPort: *listenPort,
+		ListenAddr: *listenAddr,
+	})
+	err := ashidisServer.Start()
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,36 @@
 package server
+
+import (
+	"fmt"
+	"net"
+)
+
+type AshdisServer struct {
+	AshdisServerOpts
+}
+
+type AshdisServerOpts struct {
+	ListenAddr string `envconfig:"ASHDIS_ADDR" default:"localhost"`
+	ListenPort int    `envconfig:"ASHDIS_PORT" default:"6370"`
+}
+
+func NewAshdisServer(ashdisServerOpts AshdisServerOpts) *AshdisServer {
+	return &AshdisServer{
+		AshdisServerOpts: ashdisServerOpts,
+	}
+}
+
+// test setup a basic tcp server that just listens for connections and responds Hello world
+func (a *AshdisServer) Start() error {
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{
+		IP:   net.ParseIP(a.ListenAddr),
+		Port: a.ListenPort,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s:%d: %w", a.ListenAddr, a.ListenPort, err)
+	}
+	defer listener.Close()
+
+	fmt.Printf("Listening for messages on %s:%d\n", a.ListenAddr, a.ListenPort)
+	return nil
+}
